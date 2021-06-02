3466efc06a187b098b12d2ecaaf58074e6c7bd36
Omri Webber <omriwebber@gmail.com>
2021-06-02 16:14:05 +1200

    created currentDonor struct

--- BloodBank/functions.h
+++ BloodBank/functions.h
@@ -1,7 +1,24 @@
 #include <string>
 using namespace std;
 
-void displayHowTo();
-void displayWhy();
+void line(int, char);
+void donorScreen();
 void Login(string);
 void Register(string);
+
+struct currentDonor {
+	string username;
+	string password;
+	string fname;
+	string lname;
+	string dob;
+	string nationality;
+	string ethnicity;
+	string gender;
+	string existingConditions;
+	string bloodType;
+	string contactNumber;
+	string email;
+	string address;
+	string lastDonation = " ";
+};
