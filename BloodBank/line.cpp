408edc0ba86e39c2ba6239616c60f00736bbdb23
Omri Webber <omriwebber@gmail.com>
2021-06-02 16:14:15 +1200

    line function

---
+++ BloodBank/line.cpp
@@ -0,0 +1,13 @@
+#include "functions.h"
+#include <iostream>
+
+using namespace std;
+
+void line(int n, char s) {
+	cout << "\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << s;
+	}
+	cout << "\n";
+}
