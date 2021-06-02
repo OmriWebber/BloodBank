99f577c8f5b698ea005a7c9d6521011dfba1d44c
Omri Webber <omriwebber@gmail.com>
2021-06-02 14:25:53 +1200

    Adding project files

---
+++ BloodBank/displayHowTo.cpp
@@ -0,0 +1,8 @@
+#include "functions.h"
+#include <iostream>
+
+using namespace std;
+
+void displayHowTo() {
+	cout << "How to donate blood.";
+}
---
+++ BloodBank/displayWhy.cpp
@@ -0,0 +1,8 @@
+#include "functions.h"
+#include <iostream>
+
+using namespace std;
+
+void displayWhy() {
+	cout << "Why donate blood.";
+}
