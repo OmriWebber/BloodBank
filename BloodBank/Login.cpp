843d669ac81c6130769352cc520d1fb28271eda7
Omri Webber <omriwebber@gmail.com>
2021-06-14 06:02:51 +1200

    Removed loginStatus check

--- BloodBank/Login.cpp
+++ BloodBank/Login.cpp
@@ -4,9 +4,7 @@ void Login(string cat) {
     cout << "\n";
     char inName[30], inPassword[30];
     int currentUser;
-    bool loginStatus = false;
     cin.ignore();
-
     cat[0] = toupper(cat[0]);
     cout << "\n\t\t\t\t" << cat << " Login";
     line(100, '-');
@@ -21,51 +19,37 @@ void Login(string cat) {
 
         if (cat == "Donor") {
             donorList* donor = donorData("donors.dat");
-            if (!loginStatus)
-            {
-                for (int i = 0; i < donorLimit; i++){
-                    if (strcmp(inName, donor[i].username) == 0 && strcmp(inPassword, donor[i].password) == 0)
-                    {
-                        loginStatus = true;
-                        currentUser = i;
-                        cout << "\n\nLogin Successful\n" << "Welcome, " << donor[i].fname;
-                        goto success;
-                    }
+            for (int i = 0; i < donorLimit; i++){
+                if (strcmp(inName, donor[i].username) == 0 && strcmp(inPassword, donor[i].password) == 0)
+                {
+                    currentUser = i;
+                    cout << "\n\nLogin Successful\n" << "Welcome, " << donor[i].fname;
+                    goto success;
                 }
-                
             }
         }
         else if (cat == "Recipient") {
-        recipientList* recipient = recipientData("recipients.dat");
-
-            if (!loginStatus)
-            {
-                for (int i = 0; i < recipientLimit; i++) {
-                    if (strcmp(inName, recipient[i].username) == 0 && strcmp(inPassword, recipient[i].password) == 0)
-                    {
-                        loginStatus = true;
-                        currentUser = i;
-                        cout << "\n\nLogin Successful\n" << "Welcome, " << recipient[i].name;
-                        goto success;
-                    }
+            recipientList* recipient = recipientData("recipients.dat");
+            
+            for (int i = 0; i < recipientLimit; i++) {
+                if (strcmp(inName, recipient[i].username) == 0 && strcmp(inPassword, recipient[i].password) == 0)
+                {
+                    cout << "Username " << i << ": " << recipient[i].username << endl;
+                    currentUser = i;
+                    cout << "\n\nLogin Successful\n" << "Welcome, " << recipient[i].username;
+                    goto success;
                 }
-
             }
         }
         else if (cat == "Admin") {
-        adminList* admin = adminData("admins.txt");
-            if (!loginStatus)
-            {
-                for (int i = 0; i < adminLimit; i++) {
-                    if (strcmp(inName, admin[i].username) == 0 && strcmp(inPassword, admin[i].password) == 0)
-                    {
-                        loginStatus = true;
-                        currentUser = i;
-                        cout << "\n\nLogin Successful\n" << "Welcome, " << admin[i].username;
-                        goto success;
-                    }
+            adminList* admin = adminData("admins.txt");
+            for (int i = 0; i < adminLimit; i++) {
+                if (strcmp(inName, admin[i].username) == 0 && strcmp(inPassword, admin[i].password) == 0)
+                {
+                    currentUser = i;
+                    cout << "\n\nLogin Successful\n" << "Welcome, " << admin[i].username;
+                    goto success;
                 }
-
             }
         }
         
